5d9b3e53b5de946f0e222724a9e38726e398be61
antran1245 <antran1245@gmail.com>
2022-11-15 21:33:50 -0800

    styles for tablet

--- pages/Skills.jsx
+++ pages/Skills.jsx
@@ -4,32 +4,35 @@ import styles from '../styles/Skills.module.css'
 
 export default function Skills() {
     return(
-        <section className={styles.skills}>
-            <div className={styles.box}>
-                <h2>HTML</h2>
-                <p>4 Years Experience</p>
+        <section>
+            <hr className={styles.line}/>
+            <div className={styles.skills}>
+                <div className={styles.box}>
+                    <h2>HTML</h2>
+                    <p>4 Years Experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h2>CSS</h2>
+                    <p>4 Years Experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h2>Javascript</h2>
+                    <p>4 Years Experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h2>Accessibility</h2>
+                    <p>4 Years Experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h2>React</h2>
+                    <p>3 Years Experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h2>Sass</h2>
+                    <p>3 Years Experience</p>
+                </div>
+                <Image src={rings} alt="rings" className={styles.rings}/>
             </div>
-            <div className={styles.box}>
-                <h2>CSS</h2>
-                <p>4 Years Experience</p>
-            </div>
-            <div className={styles.box}>
-                <h2>Javascript</h2>
-                <p>4 Years Experience</p>
-            </div>
-            <div className={styles.box}>
-                <h2>Accessibility</h2>
-                <p>4 Years Experience</p>
-            </div>
-            <div className={styles.box}>
-                <h2>React</h2>
-                <p>3 Years Experience</p>
-            </div>
-            <div className={styles.box}>
-                <h2>Sass</h2>
-                <p>3 Years Experience</p>
-            </div>
-            {/* <Image src={rings} alt="rings" className={styles.rings}/> */}
         </section>
     )
 }
