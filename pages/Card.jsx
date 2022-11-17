5c5f12dbb67e3395dfb6bf94679690b0d67e82a7
antran1245 <antran1245@gmail.com>
2022-11-17 15:49:08 -0800

    dealing with a bug on card map

--- pages/Card.jsx
+++ pages/Card.jsx
@@ -19,9 +19,9 @@ export default function Card({image, title, languages}) {
             </div>
             <p className={styles.title}>{title}</p>
             <p className={styles.languagesBox}>
-                {languages.map((item, index) => {
+                {language.length > 0 ?languages.map((item, index) => {
                     return <span key={index} className={styles.languages}>{item}</span>
-                })}
+                }) : null}
             </p>
             <p className={styles.links}>
                 <a href="#">VIEW PROJECT</a>
